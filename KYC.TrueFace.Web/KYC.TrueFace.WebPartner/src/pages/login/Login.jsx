51a48cac9e32e4ce41aec5fd9622a24436cf3d40
gustaeps2004 <gustaeps@gmail.com>
2026-02-26 21:38:07 -0300

    feat: adding image at login

--- KYC.TrueFace.Web/KYC.TrueFace.WebPartner/src/pages/login/Login.jsx
+++ KYC.TrueFace.Web/KYC.TrueFace.WebPartner/src/pages/login/Login.jsx
@@ -1,30 +1,52 @@
 import { FormLogin } from "../../components/Login/FormLogin"
+import ImgLogin from "../../assets/imgs/login.png"
 
 export function Login() {
   return(
     <div className="
-      bg-gray-100 
-      min-h-screen 
-      flex items-center 
-      justify-center"
+      min-h-screen
+      bg-btn-login-100
+      flex
+      items-center
+      justify-center
+      px-10"
     >
       <div className="
-        bg-primary 
-        max-w-md 
-        w-full 
-        rounded-2xl 
-        shadow-xl 
-        p-8"
+        flex 
+        items-center 
+        gap-20 
+        max-w-7xl 
+        w-full"
       >
-        <h1 className="
-          text-3xl 
-          font-bold
-          text-title
-          text-center 
-          mb-6"
+        <div className="flex-1">
+          <img 
+            src={ImgLogin} 
+            alt="login" 
+            className="
+              w-full
+              max-h-150
+              h-[85vh] object-contain"
+          />
+        </div>
+        <div className="
+          w-full
+          max-w-md
+          bg-primary
+          rounded-2xl
+          shadow-2xl
+          p-10"
         >
-          Login</h1>
-        <FormLogin />
+          <h1 className="
+            text-3xl
+            font-bold
+            text-title
+            text-center
+            mb-8"
+          >
+            Login
+          </h1>
+          <FormLogin />
+        </div>
       </div>
     </div>
   )
