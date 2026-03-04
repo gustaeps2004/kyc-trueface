771bba33cc307ddcb2adf287593a1fb1636064d4
gustaeps2004 <gustaeps@gmail.com>
2026-03-03 21:22:02 -0300

    fix: adjusting topbar

--- KYC.TrueFace.Web/KYC.TrueFace.WebPartner/src/components/base/Topbar.jsx
+++ KYC.TrueFace.Web/KYC.TrueFace.WebPartner/src/components/base/Topbar.jsx
@@ -1,12 +1,18 @@
-import { LogOut } from "lucide-react";
+import { LogOut, Moon, Sun } from "lucide-react";
 import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
 
 export default function Topbar({ name }) {
   const navigate = useNavigate();
+  const [darkMode, setDarkMode] = useState()
 
   const handleRedirect = () => {
     navigate('/login');
   };
+
+  const callSetDarkMode = () => {
+    setDarkMode(!darkMode)
+  }
 
   return (
     <div className="
@@ -18,13 +24,35 @@ export default function Topbar({ name }) {
       justify-between 
       px-8"
     >
-      <h1 className="text-2xl text-white font-medium">
+      <h1 className="text-2xl text-title font-medium">
         {name}
       </h1>
 
-      <button className="text-slate-300 hover:text-white transition">
-        <LogOut onClick={handleRedirect} className="cursor-pointer"/>
-      </button>
+      <div className="
+        justify-around 
+        m-3
+        flex
+        items-center
+        space-x-3"
+      >
+        <button 
+          title={darkMode ? "Light mode" : "Dark mode"}
+          className="
+            text-slate-300 
+            hover:text-title 
+            transition"
+        >
+          { 
+            darkMode 
+            ? <Sun onClick={callSetDarkMode} className="cursor-pointer" /> 
+            : <Moon onClick={callSetDarkMode} className="cursor-pointer" />
+          }
+        </button>
+
+        <button title="Log out" className="text-slate-300 hover:text-title transition">
+          <LogOut onClick={handleRedirect} className="cursor-pointer" />
+        </button>
+      </div>
     </div>
-  );
+  )
 }
