98e75df70b2c78285efde2c2ced96334035e4e2a
gustaeps2004 <gustaeps@gmail.com>
2026-05-02 13:02:29 -0300

    feat: adding api versioning

--- KYC.TrueFace.Core/KYC.TrueFace.Core.API/Program.cs
+++ KYC.TrueFace.Core/KYC.TrueFace.Core.API/Program.cs
@@ -1,9 +1,9 @@
 using KYC.TrueFace.Core.Infra.Data.Data;
+using KYC.TrueFace.Core.Infra.Ioc.Configs;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Configuration
     .AddEnvironmentVariables()
     .AddEnvironmentVariables("KYC_")
@@ -15,14 +15,15 @@ var connectionString = builder.Configuration["StrConn"];
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 
+builder.Services.ConfigureVersioning();
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
---
+++ KYC.TrueFace.Core/KYC.TrueFace.Core.Infra.Ioc/Configs/VersioningConfig.cs
@@ -0,0 +1,23 @@
+﻿using Asp.Versioning;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KYC.TrueFace.Core.Infra.Ioc.Configs;
+
+public static class VersioningConfig
+{
+    public static void ConfigureVersioning(this IServiceCollection services)
+    {
+        services.AddApiVersioning(options =>
+        {
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.ReportApiVersions = true;
+            options.ApiVersionReader = new UrlSegmentApiVersionReader();
+        })
+        .AddApiExplorer(options =>
+        {
+            options.GroupNameFormat = "'v'VVV";
+            options.SubstituteApiVersionInUrl = true;
+        });
+    }
+}
