b0f49ab4ce326bf05b81553d6ff97e1162501e6e
gustaeps2004 <gustaeps@gmail.com>
2026-09-16 20:19:05 -0300

    chore: addind health check endpoint

---
+++ backend/KYC.TrueFace.Core.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,14 @@
+﻿using KYC.TrueFace.Core.API.Controllers.Base;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KYC.TrueFace.Core.API.Controllers;
+
+[AllowAnonymous]
+[Route("api/health-check")]
+public class HealthCheckController : BaseController
+{
+    [HttpGet]
+    public IActionResult HealthCheck()
+        => Ok();
+}
